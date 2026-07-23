4a03f3e0c7edb74a9300ced5deb261c98189b333
OurOrigin Engineering <engineering@ourorigin.com>
2026-07-23 12:04:28 +0530

    Fix syntax error causing create new quote to fail, and remove preset buttons

--- app.js
+++ app.js
@@ -705,7 +705,7 @@ function syncDoc(subtotal, discount, afterDiscount, taxable, cgst, sgst, grand, 
             <tr>
               <td style="padding: 5px 8px 5px 0; border-bottom: 1px solid #eaeaea; font-weight: 600; vertical-align: top; width: 35%;">${itemName}</td>
               <td style="padding: 5px 8px; border-bottom: 1px solid #eaeaea; vertical-align: top;">${item.desc}</td>
-              <td style="padding: 5px 0; border-bottom: 1px solid #eaeaea; font-weight: 600; vertical-align: top; text-align: right; color: var(--brand-navy); white-space: nowrap;">₹${formatAmt(item.rate)} / pc</td>
+              <td style="padding: 5px 0; border-bottom: 1px solid #eaeaea; font-weight: 600; vertical-align: top; text-align: right; color: var(--brand-navy); white-space: nowrap;">₹${fmt(item.rate)} / pc</td>
             </tr>
           `;
         });
